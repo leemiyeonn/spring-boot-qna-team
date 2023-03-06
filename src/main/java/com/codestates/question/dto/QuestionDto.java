fad4247d47003a0f95146b460acad62251acdcad
leemi <lemingul7@gmail.com>
2023-03-06 18:29:22 +0900

    template

---
+++ src/main/java/com/codestates/question/dto/QuestionDto.java
@@ -0,0 +1,11 @@
+package com.codestates.question.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class QuestionDto {
+}
---
+++ src/main/java/com/codestates/question/service/QuestionService.java
@@ -0,0 +1,9 @@
+package com.codestates.question.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional // org.springframework.transaction.annotation.Transactional
+@Service
+public class QuestionService {
+}
