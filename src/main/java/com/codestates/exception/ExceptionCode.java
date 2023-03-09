d42672b1c43ec2eaaea7641f52a634f2f30ae68e
seunghyun711 <seunghyun711@gmail.com>
2023-03-10 00:07:53 +0900

    예외 처리 클래스 생성

---
+++ src/main/java/com/codestates/exception/ExceptionCode.java
@@ -0,0 +1,20 @@
+package com.codestates.exception;
+
+import lombok.Getter;
+
+public enum ExceptionCode {
+    MEMBER_NOT_FOUND(404,"MEMBER NOT FOUND"),
+    MEMBER_EXISTS(404,"MEMBER EXISTS"),
+    CANNOT_REGISTRATION_QUESTION(404, "MANAGER CANNOT REGISTRATION QUESTION");
+
+    @Getter
+    private int status;
+
+    @Getter
+    private String message;
+
+    ExceptionCode(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+}
